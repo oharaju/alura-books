3bb8ae730838e03022ce98be528ebb1a2924e538
Júlia Ohara <juliaohara.br@gmail.com>
2022-11-21 23:02:00 -0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import {Menu, Logo, TextLogo} from './styles';
+
+const Header = () => (
+  <>
+  <Menu>
+    <Logo src="./images/logo.svg" />
+    <TextLogo>AluraBooks</TextLogo>
+  </Menu>
+  </>
+);
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Menu = styled.header`
+  background-color: #f0f8ff4d;
+  border: 1px solid #e9e0e0b0;
+  border-radius: 4px;
+  padding: 30px;
+  display: flex;
+`
+
+export const Logo = styled.img`
+`;
+
+export const TextLogo = styled.p`
+`
