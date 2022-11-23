a718cea1dea8f09f508aad1e37005c660244c3f7
Júlia Ohara <juliaohara.br@gmail.com>
2022-11-22 22:21:02 -0300

    feat: add reset css file

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,10 +2,10 @@ import {Menu, Logo, TextLogo} from './styles';
 
 const Header = () => (
   <>
-  <Menu>
-    <Logo src="./images/logo.svg" />
-    <TextLogo>AluraBooks</TextLogo>
-  </Menu>
+    <Menu>
+      <Logo src="./images/logo.svg" />
+      <TextLogo>AluraBooks</TextLogo>
+    </Menu>
   </>
 );
 
